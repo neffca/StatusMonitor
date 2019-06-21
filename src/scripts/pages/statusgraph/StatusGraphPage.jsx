bc65400a09d2326900c9efcc0eeb5969bde23568
neffca <caitlinneff@gmail.com>
2019-06-20 22:00:08 -0400

    updated to deploy to git pages

---
+++ src/scripts/pages/statusgraph/StatusGraphPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class StatusGraphPage extends React.Component {
+
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        {"graph"}
+      </div>
+    );
+  }
+}
+
+export default StatusGraphPage;
