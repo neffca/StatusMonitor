6ef67e2eaaf34a9e3b0df356f2f7081a4dce6ce7
neffca <caitlinneff@gmail.com>
2019-07-07 21:31:07 -0400

    Updates

--- src/scripts/actions/statusAction.js
+++
@@ -1,8 +0,0 @@
-import {UPDATE_STATUS} from '../constants/action-types';
-
-export function updateStatus(payload) {
-  return {
-    type: UPDATE_STATUS,
-    payload
-  }
-}
--- src/scripts/store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from 'redux';
-import statusReducer from "../reducers/statusReducer";
-
-function configureStore() {
-  return createStore(statusReducer);
-}
-
-export default configureStore;
