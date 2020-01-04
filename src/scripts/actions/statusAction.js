6ff0837a747c7701b0a59d0119d9ca055e8db99e
neffca <caitlinneff@gmail.com>
2020-01-03 22:30:46 -0500

    Updates

--- src/scripts/actions/statusAction.js
+++
@@ -1,6 +0,0 @@
-import {UPDATE_STATUS} from '../constants/action-types';
-
-export const updateStatus = (payload) => ({
-    type: UPDATE_STATUS,
-    payload
-  });
--- src/scripts/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import statusReducer from './statusReducer';
-import {combineReducers} from 'redux';
-
-const rootReducer = combineReducers({
-    statusReducer
-});
-
-export default rootReducer;
