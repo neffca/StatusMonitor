9519898ea4d2e94222d1d57c01dc81eb1d347ade
neffca <caitlinneff@gmail.com>
2020-09-30 22:42:44 -0400

    moved server files into server directory

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const publicPath = path.join(__dirname, '../client', 'public');
+const { DATABASE_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+});
 
 app.use(express.static(publicPath));
 app.use(cors())
